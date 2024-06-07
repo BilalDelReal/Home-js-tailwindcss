f34a58234534d55b41fdb854aa559a3f41c30a64
bilaldelreal <bilaldelrealouldam@hotmail.com>
2024-06-07 23:53:36 +0200

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html"],
+    theme: {
+
+        colors: {
+            'primary-orange': 'hsl(35, 77%, 62%)',
+            'prymary-red': 'hsl(5, 85%, 63%)',
+            'off-white': 'hsl(36, 100%, 99%)',
+            'dark-blue': 'hsl(236, 13%, 42%)',
+            'blue': 'hsl(233, 8%, 79%)',
+            'very-dark-blue': 'hsl(240, 100%, 5%)'
+        }
+
+    },
+    plugins: [],
+}
+
